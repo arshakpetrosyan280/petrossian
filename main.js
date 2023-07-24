75a491ef6f4e8b10045050c687ebb6009c0a0c1c
arshakpetrosyan280 <arshakpetrosyan280@gmail.com>
2023-07-24 14:21:25 +0400

    a

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.get('/', async function(req, res) {
     if(user === undefined){
       res.sendFile(__dirname + "/public/pages/index.html");
     }else{
-      res.redirect("/home/" + user.id);
+      res.redirect(200, "/home/" + user.id);
     }
 });
 app.post('/', async function(req, res) { 
