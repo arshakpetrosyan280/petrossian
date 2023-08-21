6ef4ec07003101e01411c6954a36c396f0c3fd8f
arshakpetrosyan280 <arshakpetrosyan280@gmail.com>
2023-08-21 17:12:44 +0400

    ape

--- public/js/home.js
+++ public/js/home.js
@@ -4,14 +4,23 @@ fetch("/session-user")
 	.then(data=>data.json())
 	.then(user => {
 		if(user){
-			usernameTag.innerHTML = `${user.firstName} ${user.lastname}`
+			usernameTag.setAttribute("href", `/account/${user.id}`);
+			usernameTag.innerHTML = `
+				<i class="fa fa-user"></i>&nbsp;
+				${user.firstName} ${user.lastname}
+			`;
 		}
 		
 	}).catch((error) => {
 	  	fetch("/cookie-user")
 			.then(data=>data.json())
 			.then(user => {
-				usernameTag.innerHTML = `${user.firstName} ${user.lastname}`
+				console.log(user);
+				usernameTag.setAttribute("href", `/account/${user.id}`);
+				usernameTag.innerHTML = `
+					<i class="fa fa-user"></i>&nbsp;
+					${user.firstName} ${user.lastname}
+				`;
 			});
 	});
 
